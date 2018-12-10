c4fbe29100f078c84b9fcedb4310e9ba210cb03b
Rayan26 <kallabrayan@gmail.com>
2018-12-10 14:38:46 +0100

    Création de la classe SparseRasterImage et de ces constructeurs et méthodes. On le définit que getPixel et setPixel car toute les autres sont dejas définit dans la classe étendue.

--- src/main/java/image/SparseRasterImage.java
+++ src/main/java/image/SparseRasterImage.java
@@ -2,13 +2,17 @@ package image;
 
 import javafx.scene.paint.Color;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public  class SparseRasterImage extends RasterImage {
     public  Color color;
+    Map<Point,Color> pixelMap;
 
     public SparseRasterImage(Color color, int width, int height){
+
         super(color,width,height);
+
     }
 
     public SparseRasterImage(Color[][] pixels){
@@ -19,17 +23,19 @@ public  class SparseRasterImage extends RasterImage {
 
 
     public void createRepresentation() {
-       //
+        pixelMap = new HashMap<>();
     }
 
 
     public void setPixelColor(Color color, int x, int y) {
-        //
+        pixelMap.put(new Point(x,y),color);
+
     }
 
 
     public Color getPixelColor(int x, int y) {
-        return null;
+
+        return pixelMap.getOrDefault(new Point(x,y),Color.WHITE);
     }
 
 
